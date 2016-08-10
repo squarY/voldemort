3d1cdf530ad43cd94266d7b44b74f4c1290117c2
Yan Yan <yayan@linkedin.com>
2016-08-09 19:18:32 -0700

    Fix: Extend admin request time out from 1min to 5min. Add more logs when handing failed fetch request.

--- contrib/hadoop-store-builder/src/java/voldemort/store/readonly/mr/azkaban/VoldemortSwapJob.java
+++ contrib/hadoop-store-builder/src/java/voldemort/store/readonly/mr/azkaban/VoldemortSwapJob.java
@@ -120,9 +120,9 @@ public class VoldemortSwapJob extends AbstractJob {
         }
 
         AdminClientConfig adminConfig = new AdminClientConfig().setMaxConnectionsPerNode(cluster.getNumberOfNodes())
-                                                               .setAdminConnectionTimeoutSec(15)
+                                                               .setAdminConnectionTimeoutSec(60)
                                                                .setMaxBackoffDelayMs(maxBackoffDelayMs)
-                                                               .setAdminSocketTimeoutSec(60);
+                                                               .setAdminSocketTimeoutSec(60 * 5);// Admin request time out is 5min.
 
         ClientConfig clientConfig = new ClientConfig().setBootstrapUrls(cluster.getBootStrapUrls())
                                                       .setConnectionTimeout(httpTimeoutMs,
